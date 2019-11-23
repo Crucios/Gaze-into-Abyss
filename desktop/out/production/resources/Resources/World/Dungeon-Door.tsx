03eeab1c6c15276476c54c763b316aa360765a3f
tensa132 <51240391+tensa132@users.noreply.github.com>
2019-11-23 22:03:30 +0700

    MainMenu&GameScreen
    Penambahan Aset Main Menu, Main Menu sudah terconnect dengan GameScreen

---
+++ desktop/out/production/resources/Resources/World/Dungeon-Door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="Dungeon-Door" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="3520" columns="64">
+ <image source="dungeon-door.png" width="1152" height="1000"/>
+</tileset>
---
+++ desktop/out/production/resources/Resources/World/torch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="torch" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="1156" columns="34">
+ <image source="torches.png" width="614" height="614"/>
+</tileset>
