fd4044bbce7a6166398e77c8119f291766389a90
AndrewKur48 <47960936+AndrewKur48@users.noreply.github.com>
2019-11-23 22:10:12 +0700

    Merge branch 'master' of https://github.com/Crucios/Gaze-into-Abyss

--- core/src/Sprites/Player.java
+++ core/src/Sprites/Player.java
@@ -1,6 +1,8 @@
 package Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -8,18 +10,28 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.gazeintoabyss.GazeintoAbyss;
 
+import Screens.PlayScreen;
+
 public class Player extends Sprite{
 	public World world;
 	public Body b2body;
+	private TextureRegion playerStand;
 	
-	public Player(World world) {
+	private Vector2 position;
+	
+	public Player(World world, PlayScreen screen,Vector2 position) {
+		super(screen.getAtlas().findRegion("sprite-player"));
 		this.world = world;
+		this.position = position;
 		definePlayer();
+		playerStand = new TextureRegion(getTexture(), 0,0,16,16);
+		setBounds(0,0,16,16);
+		setRegion(playerStand);
 	}
 	
 	public void definePlayer() {
 		BodyDef bdef = new BodyDef();
-		bdef.position.set(129 / GazeintoAbyss.PPM,100 / GazeintoAbyss.PPM);
+		bdef.position.set(position.x / GazeintoAbyss.PPM,position.y / GazeintoAbyss.PPM);
 		bdef.type = BodyDef.BodyType.DynamicBody;
 		b2body = world.createBody(bdef);
 		
