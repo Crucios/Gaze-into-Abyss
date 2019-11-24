2b17209868ba5bbf11b4dfc5c3e8a17ad608425d
Crucios <57003452+Crucios@users.noreply.github.com>
2019-11-24 19:35:40 +0700

    Update WorldCreator.java

--- core/src/Tools/WorldCreator.java
+++ core/src/Tools/WorldCreator.java
@@ -9,10 +9,10 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
-import com.mygdx.gazeintoabyss.GazeintoAbyss;
 
 import Sprites.Chest;
 import Sprites.DoorArea;
+import Sprites.Ground;
 
 public class WorldCreator {
 	public WorldCreator(World world,TiledMap map) {
@@ -25,29 +25,20 @@ public class WorldCreator {
 		for(MapObject object : map.getLayers().get("ground-object").getObjects().getByType(RectangleMapObject.class)) {
 			Rectangle rect = ((RectangleMapObject) object).getRectangle();
 			
-			bdef.type = BodyDef.BodyType.StaticBody;
-			bdef.position.set((rect.getX() + rect.getWidth()/2) / GazeintoAbyss.PPM,(rect.getY() + rect.getHeight()/2) / GazeintoAbyss.PPM);
-			
-			body = world.createBody(bdef);
-			
-			shape.setAsBox((rect.getWidth()/2) / GazeintoAbyss.PPM, (rect.getHeight()/2) / GazeintoAbyss.PPM);
-			fdef.shape = shape;
-			fdef.filter.categoryBits = 4;
-			fdef.filter.maskBits = 1 | 2;
-			body.createFixture(fdef);
+			new Ground(world, map, rect);
 		}
 		
 		//Set for chest-object
 		for(MapObject object : map.getLayers().get("chest-object").getObjects().getByType(RectangleMapObject.class)) {
 			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			
 			new Chest(world, map, rect);
 		}
 		
 		//Set for door-area-object
 		for(MapObject object : map.getLayers().get("door-area-object").getObjects().getByType(RectangleMapObject.class)) {
 			Rectangle rect = ((RectangleMapObject) object).getRectangle();
-			fdef.filter.categoryBits=1;
-			fdef.filter.maskBits=2;
+			
 			new DoorArea(world, map, rect);
 		}
 	}
